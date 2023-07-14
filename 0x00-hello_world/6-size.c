e0caa53b5c7af8c06316a3f0b75e816a63ab386f
Kwame <mawusikwame009@gmail.com>
2023-07-14 04:42:19 -0700

    First C lesson

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - the function that prints the sizeof each 
+ * main - the function that prints the sizeof each
  * data type listed below using the sizeof() operator
  * depending on the architecture of the cpu.
  * Return: 0
@@ -15,6 +15,5 @@ int main(void)
 	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-
 	return (0);
 }
