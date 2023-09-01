82a3fcc63ca41438215afbb7e1828a721b1bf935
Kwame <mawusikwame009@gmail.com>
2023-08-31 17:46:32 -0700

    bit manipulation

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
-
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
+ * print_listint - a function that prints all the elements
+ * of a list
+ * @h: a pointer to a list
+ * Return: a pointer to a struct
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	int nodeNum = 0;
 
-	while (h)
+	while (h != NULL)
 	{
+		printf("[%d]\n", h->n);
 		printf("%d\n", h->n);
-		num++;
 		h = h->next;
+		nodeNum++;
 	}
-
-	return (num);
+	return (nodeNum);
 }
